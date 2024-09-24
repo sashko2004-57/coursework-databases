044ff902c49b0bc74f6f5853f32eef80c585fbe2
Vanilkaa <gamvanin@gmail.com>
2024-09-24 22:33:38 +0300

    Fix publish.sh

--- publish.sh
+++ publish.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/Vanilkaa/coursework-databases master:gh-pages
+git push -f git@github.com:Vanilkaa/coursework-databases.git master:gh-pages
 
 cd -
